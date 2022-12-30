e62ff2b434e9e5e06949e5d118d4cd416680607b
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-12-30 15:04:35 +0600

    ok

---
+++ forLoop.js
@@ -0,0 +1,10 @@
+// for (var x = 1; x <= 100 ; x = x+1) {
+//   document.write(' '+x)
+// }
+
+
+// sum = 0;
+// for (var x = 1; x <= 5; x++){
+// sum = sum + x;
+// }
+// console.log(sum)
--- main.js
+++
@@ -1,3 +0,0 @@
-for (var x = 1; x <= 100 ; x = x+1) {
-  document.write(' '+x)
-}
